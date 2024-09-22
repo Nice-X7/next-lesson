6177490bffe2e9d6e5707823bd21abfabeae0f86
Muhammad <777soer@gmail.com>
2024-09-22 14:17:12 +0300

    recode a routes

--- src/app/api/html/route.ts
+++ src/app/api/html/route.ts
@@ -7,23 +7,23 @@ export async function GET(req: NextRequest) {
 
 export async function POST(req: NextRequest) {
     try {
-        const body = await req.json();
+        const {question, selectAnswer} = await req.json();
 
-        if (!body.question || !body.options || !body.correctAnswer) {
-            return NextResponse.json({ error: "Все поля обязательны" }, { status: 400 });
+        const foundQuestion = htmlData.find(q => q.question === question)
+
+        if (!foundQuestion) {
+            return NextResponse.json(
+                {message: 'Вопрос не найден'},
+                {status: 404}
+            )
         }
+        const isCorrect = foundQuestion.correctAnswer === selectAnswer
 
-        const newQuestion = {
-            question: body.question,
-            options: body.options,
-            correctAnswer: body.correctAnswer
-        };
+        return NextResponse.json({
+            message: isCorrect ? 'Правильный ответ' : 'Неправильный ответ'
+        })
 
-        htmlData.push(newQuestion);
-
-        return NextResponse.json({ message: "Вопрос успешно добавлен", data: newQuestion }, { status: 201 });
-
-    } catch (error) {
-        return NextResponse.json({ error: "Неверный формат данных" }, { status: 400 });
+    } catch (error: any) {
+        return NextResponse.json({ error: "Неверный формат данных" }, { status: 500 });
     }
 }
